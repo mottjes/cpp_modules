a8c64fb324e914ec985049689fcdd1f3fa4be1a1
Mika Ottjes <mottjes@c2r12s2.42wolfsburg.de>
2024-03-25 18:36:41 +0100

    ex00

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+void	megaphone(int argc, char **argv)
+{
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; j < (int)strlen(argv[i]); j++)
+			std:: cout << (char)toupper(argv[i][j]); 
+	}
+	std:: cout << std::endl;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		std:: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+		megaphone(argc, argv);
+	
+}
