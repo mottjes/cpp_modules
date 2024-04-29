f07e65ae311c6887dd8403fd520cc6c642e93336
Mika Ottjes <mottjes@c2r11s4.42wolfsburg.de>
2024-04-29 16:22:25 +0200

    started ex00 module_03

---
+++ module_03/ex00/ClapTrap.cpp
@@ -0,0 +1,73 @@
+#include "ClapTrap.hpp"
+
+//	Default constructor
+ClapTrap::ClapTrap() : _name("null"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "Default Constructor called" << std::endl;
+}
+
+//	Parameterized constructor
+ClapTrap::ClapTrap(std::string name) :  _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "Parameterized Constructor called" << std::endl;
+}
+
+//	Copy constuctor
+ClapTrap::ClapTrap(const ClapTrap &rhs)
+{
+	std::cout << "Copy Constructor called" << std::endl;
+	*this = rhs;
+}
+
+//	Copy assignment operator
+ClapTrap& ClapTrap::operator=(const ClapTrap &rhs)
+{
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_hitPoints = rhs._hitPoints;
+		this->_energyPoints = rhs._energyPoints;
+		this->_attackDamage = rhs._attackDamage;
+	}
+	return *this;
+}
+
+//	Destructor
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (this->_energyPoints && this->_hitPoints)
+	{
+		std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " Points of damage!" << std::endl;
+		this->_energyPoints--;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " can't attack!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->_hitPoints)
+	{
+		this->_hitPoints -= amount;
+		std::cout << "ClapTrap " << this->_name << " takes " << amount << " damage!" << std::endl;
+	}
+	else
+		std::cout << "ClapTrap don't has hit points anymore!" << std::endl; 
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_energyPoints && this->_hitPoints)
+	{
+		std::cout << "ClapTrap " << this->_name << " repairs itself and gets " << amount << " hit points!" << std::endl;
+		this->_hitPoints += amount;
+		this->_energyPoints--;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " can't repair!" << std::endl;
+}
---
+++ module_03/ex00/ClapTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	std::string	_name;
+	int			_hitPoints;
+	int			_energyPoints;
+	int			_attackDamage;
+
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &rhs);
+	ClapTrap& operator=(const ClapTrap &rhs);
+	~ClapTrap();
+
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
+#endif
